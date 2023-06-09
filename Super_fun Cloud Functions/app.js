445ea148fe9a80dc42776e385c1f61379a3a058b
Dor Levi <dorlev1204@gmail.com>
2023-06-09 12:19:49 +0300

    function getDepartmentsByOrder working good

--- Super_fun Cloud Functions/app.js
+++ Super_fun Cloud Functions/app.js
@@ -68,18 +68,18 @@ exports.getDepartmentItems = functions.https.onCall(async (data, context) => {
             return [];
         }
 
-        const productListString = client.products;
+        const productString = client.products;
 
         // Remove the opening and closing curly braces
-        const trimmedString = productListString.slice(1, -1);
+        const trimmedString = productString.slice(1, -1);
 
         // Split the string by comma and trim whitespace from each element
-        const productListArray = trimmedString.split(',').map(item => item.trim());
+        const productArray = trimmedString.split(',').map(item => item.trim());
 
         const departmentSnapshot = await db.ref(`data/Departments/'${departmentName}'/items`).once("value");
         const departmentItems = departmentSnapshot.val() || [];
 
-        const departmentProducts = productListArray.filter(product => departmentItems.includes(product));
+        const departmentProducts = productArray.filter(product => departmentItems.includes(product));
 
         return departmentProducts;
 
@@ -88,92 +88,6 @@ exports.getDepartmentItems = functions.https.onCall(async (data, context) => {
         throw new functions.https.HttpsError("internal", error.message);
     }
 });
-
-/*exports.getDepartmentsByOrder = functions.https.onCall(async (data, context) => {
-    try {
-        // Get the ID of the user from the data parameter
-        const userId = data.userId;
-
-        // Create a reference to the database
-        const db = admin.database();
-
-        // Retrieve the client node based on the provided userId
-        const clientsSnapshot = await db.ref("data/Clients").child(userId).once("value");
-        const client = clientsSnapshot.val();
-
-        if (!client || !client.products) {
-            return [];
-        }
-
-        const productListString = client.products;
-
-        // Remove the opening and closing curly braces
-        const trimmedString = productListString.slice(1, -1);
-
-        // Split the string by comma and trim whitespace from each element
-        const productListArray = trimmedString.split(',').map(item => item.trim());
-
-        // Prepare data to pass to getGroceryKeys function
-        const getGroceryKeysData = { products: productListArray };
-
-        // Invoke getGroceryKeys function using the Firebase Admin SDK
-        const departmentsOnRoute = await admin.app().functions('us-central1-superfun-84e11').httpsCallable('getGroceryKeys')(getGroceryKeysData);
-
-        return Array.from(departmentsOnRoute);
-
-    } catch (error) {
-        console.error(error);
-        throw new functions.https.HttpsError("internal", error.message);
-    }
-});*/
-
-/*exports.getDepartmentsByOrder = functions.https.onCall(async (data, context) => {
-    try {
-        // Get the ID of the user from the data parameter
-        const userId = data.userId;
-
-        // Create a reference to the database
-        const db = admin.database();
-
-        // Retrieve the client node based on the provided userId
-        const clientsSnapshot = await db.ref("data/Clients").child(userId).once("value");
-        const client = clientsSnapshot.val();
-
-        if (!client || !client.products) {
-            return [aaaaaa];
-        }
-
-        const productListString = client.products;
-
-        // Remove the opening and closing curly braces
-        const trimmedString = productListString.slice(1, -1);
-
-        // Split the string by comma and trim whitespace from each element
-        const productListArray = trimmedString.split(',').map(item => item.trim());
-
-        // Prepare data to pass to getGroceryKeys function
-        const getGroceryKeysData = { products: productListArray };
-
-        // Make an HTTP request to the target cloud function's URL
-        const url = 'https://us-central1-superfun-84e11.cloudfunctions.net/getGroceryKeys';
-        const queryString = Object.entries(getGroceryKeysData)
-            .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-            .join('&');
-
-        const response = await fetch(`${url}?${queryString}`, {
-            method: 'GET',
-        });
-
-        // Parse the response from the target cloud function
-        const departmentsOnRoute = await response.json();
-
-        return Array.from(departmentsOnRoute);
-
-    } catch (error) {
-        console.error(error);
-        throw new functions.https.HttpsError("internal", error.message);
-    }
-});*/
 
 exports.getDepartmentsByOrder = functions.https.onCall(async (data, context) => {
     try {
@@ -187,33 +101,22 @@ exports.getDepartmentsByOrder = functions.https.onCall(async (data, context) => 
         const clientsSnapshot = await db.ref("data/Clients").child(userId).once("value");
         const client = clientsSnapshot.val();
 
-        if (!client || !client.products) {
+        if (!client || !client.all_departments) {
             return [];
         }
 
-        const productListString = client.products;
+        const departmentListString = client.all_departments;
 
         // Remove the opening and closing curly braces
-        const trimmedString = productListString.slice(1, -1);
+        const trimmedString = departmentListString.slice(1, -1);
 
         // Split the string by comma and trim whitespace from each element
-        const productListArray = trimmedString.split(',').map(item => item.trim());
+        const departmentArray = trimmedString.split(',').map(item => item.trim());
 
-        // Prepare data to pass to getGroceryKeys function
-        const getGroceryKeysData = { products: productListArray };
-
-        // Invoke getGroceryKeys function using the Firebase Admin SDK
-        const getGroceryKeys = admin.functions().httpsCallable('getGroceryKeys');
-        const departmentsOnRoute = await getGroceryKeys(getGroceryKeysData);
-
-        return Array.from(departmentsOnRoute.data);
+        return departmentArray;
 
     } catch (error) {
         console.error(error);
         throw new functions.https.HttpsError("internal", error.message);
     }
 });
-
-
-
-
